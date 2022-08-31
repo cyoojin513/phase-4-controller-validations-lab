5227174657cf6ce79addaf84979dc0d8a6fd293e
yoojin <yoojin9005@gmail.com>
2022-08-31 00:12:24 -0400

    commit tree 03bd6f38e096b4c44ec8356b066601797f964f05

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  private
+
+  def render_not_found(error)
+    render json: { errors: error.message }, status: :not_found
+  end
+
+  def render_invalid(invalid)
+    render json: { errors: invalid.record.errors.messages }, status: :unprocessable_entity
+  end
 end
